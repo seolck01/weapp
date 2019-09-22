61314bf1d390091fe1aa743e6275f6703ca52ed5
Charles <928196891@qq.com>
2019-09-22 22:33:16 +0800

    修改重置数据库

--- tools/cAuth.sql
+++ tools/cAuth.sql
@@ -35,3 +35,35 @@ CREATE TABLE `cSessionInfo` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='会话管理用户信息';
 
 SET FOREIGN_KEY_CHECKS = 1;
+
+
+
+
+DROP TABLE IF EXISTS `book`;
+
+ CREATE TABLE `book` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `isbn` varchar(20) NOT NULL,
+  `openid` varchar(100) NOT NULL,
+  `title` varchar(100) NOT NULL,
+  `pic` varchar(100) NOT NULL,
+  `summary` varchar(1000) NOT NULL,
+  `price` varchar(100) DEFAULT NULL,
+  `author` varchar(100) DEFAULT NULL,
+  `count` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+
+DROP TABLE IF EXISTS `comments`;
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `openid` varchar(100) NOT NULL,
+  `bookid` varchar(100) NOT NULL,
+  `comment` varchar(200) NOT NULL,
+  `phone` varchar(20) DEFAULT NULL,
+  `location` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+
